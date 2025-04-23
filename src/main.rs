002eb0f5412e266381ac469741abb70d8ecb6da2
Lxxr <counull@outlook.com>
2025-04-23 10:06:40 +0800

    feat:Hello server with tonic example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,42 @@
-fn main() {
+mod proto_gen {
+    pub mod helloworld;
+}
+
+
+use tonic::{transport::Server, Request, Response, Status};
+use proto_gen::helloworld::{HelloRequest, HelloReply, greeter_server::{Greeter, GreeterServer}};
+
+
+#[derive(Default)]
+pub struct MyGreeter {}
+
+#[tonic::async_trait]
+impl Greeter for MyGreeter {
+    async fn say_hello(
+        &self,
+        request: Request<HelloRequest>,
+    ) -> Result<Response<HelloReply>, Status> {
+        println!("Got a request from {:?}", request.remote_addr());
+
+        let reply = HelloReply {
+            message: format!("Hello {}!", request.into_inner().name),
+        };
+        Ok(Response::new(reply))
+    }
+}
+
+
+#[tokio::main]
+async fn main()  -> Result<(), Box<dyn std::error::Error>>{
+    let addr = "[::1]:50051".parse().unwrap();
+    let greeter = MyGreeter::default();
+    println!("GreeterServer listening on {}", addr);
     println!("Hello, world!");
+
+    Server::builder()
+        .add_service(GreeterServer::new(greeter))
+        .serve(addr)
+        .await?;
+
+    Ok(())
 }
